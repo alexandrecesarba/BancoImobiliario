24ef9e51d1fe5850ab65dca13f33805eaef9fd11
Heloisa <helopessoa987@icloud.com>
2022-06-04 17:13:06 -0300

    singleton on baralho

--- src/model/BaralhoSorte.java
+++ src/model/BaralhoSorte.java
@@ -3,41 +3,18 @@ package model;
 import java.util.ArrayList;
 import java.util.Collections;
 
-import model.CartaSorteReves.SorteReves;
-
 class BaralhoSorte {
-	private ArrayList<SorteReves> baralho = new ArrayList<SorteReves>();
-	BaralhoSorte(){
-		baralho.add(SorteReves.Chance1);
-		baralho.add(SorteReves.Chance2);
-		baralho.add(SorteReves.Chance3);
-		baralho.add(SorteReves.Chance4);
-		baralho.add(SorteReves.Chance5);
-		baralho.add(SorteReves.Chance6);
-		baralho.add(SorteReves.Chance7);
-		baralho.add(SorteReves.Chance8);
-		baralho.add(SorteReves.Chance9);
-		baralho.add(SorteReves.Chance10);
-		baralho.add(SorteReves.Chance11); 
-		baralho.add(SorteReves.Chance12); 
-		baralho.add(SorteReves.Chance13);
-		baralho.add(SorteReves.Chance14);
-		baralho.add(SorteReves.Chance15);
-		baralho.add(SorteReves.Chance16);
-		baralho.add(SorteReves.Chance17);
-		baralho.add(SorteReves.Chance18);
-		baralho.add(SorteReves.Chance19);
-		baralho.add(SorteReves.Chance20);
-		baralho.add(SorteReves.Chance21);
-		baralho.add(SorteReves.Chance22);
-		baralho.add(SorteReves.Chance23);
-		baralho.add(SorteReves.Chance24);
-		baralho.add(SorteReves.Chance25);
-		baralho.add(SorteReves.Chance26);
-		baralho.add(SorteReves.Chance27);
-		baralho.add(SorteReves.Chance28);
-		baralho.add(SorteReves.Chance29); 
-		baralho.add(SorteReves.Chance30);
+	private ArrayList<Integer> baralho = new ArrayList<Integer>();
+	private BaralhoSorte instance = new BaralhoSorte();
+	
+	BaralhoSorte getInstance() {
+		return this.instance;
+	}
+	
+	private BaralhoSorte(){
+		for(int i=0; i <= 30; i++) {
+			baralho.add(i);
+		}
 		
 		Collections.shuffle(baralho);
 	}
--- src/model/CartaSorteReves.java
+++ src/model/CartaSorteReves.java
@@ -6,110 +6,15 @@ class CartaSorteReves {
 	private CartaSorteReves(){	}
 	
 	CartaSorteReves getInstance() {
-		return this.instance;
+		 return this.instance;
 	}
+	private int efeito[] = new int[]{25,150,80,200,50,50,100,100,0,200,1,45,100,100,20,-15,-25,-45,-30,-100,-100,-40,-1,-30,-50,-25,-30,-45,-50,-50};
+	// 0 -> saída livre da prisão
+	// 1 -> receba 50 de cada jogador
+	//-1 -> vá para a prisão
 	
-	int getEfeito(SorteReves carta) {
-		switch(carta) {
-		case Chance1:
-			return 25;			
-			
-		case Chance2:
-			return 150;			
-			
-		case Chance3:
-			return 80;			
-			
-		case Chance4:
-			return 200;			
-			
-		case Chance5:
-			return 50;
-					
-		case Chance6:
-			return 50;		
-			
-		case Chance7:
-			return 100;			
-			
-		case Chance8:
-			return 100;
-						
-		case Chance9:
-			return 0; //saída livre da prisão			
-			
-		case Chance10:
-			return 200;			
-			
-		case Chance11:
-			return 1; //receba 50 de cada jogador			
-			
-		case Chance12:
-			return 45;
-						
-		case Chance13:
-			return 100;			
-			
-		case Chance14:
-			return 100;			
-			
-		case Chance15:
-			return 20;			
-	
-		case Chance16:
-			return -15;		
-			
-		case Chance17:
-			return -25;
-			
-		case Chance18:
-			return -45;			
-			
-		case Chance19:
-			return -30;			
-			
-		case Chance20:
-			return -100;
-			
-		case Chance21:
-			return -100;
-			
-		case Chance22:
-			return -40;
-			
-		case Chance23:
-			return -1; //va para a prisao
-			
-		case Chance24:
-			return -30;
-			
-		case Chance25:
-			return -50;
-			
-		case Chance26:
-			return -25;
-		
-		case Chance27:
-			return -30;
-
-		case Chance28:
-			return -45;
-			
-		case Chance29:
-			return -50;
-
-		case Chance30:
-			return -50;
-			
-		}
-		return 8888;
-		
+	int getEfeito(int carta) {
+		return efeito[carta];
 	}
-	
-	
-	
-	
-	
-
 
 }
