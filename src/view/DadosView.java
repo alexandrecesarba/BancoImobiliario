99f682da59d9561559bd5bb75199182acac48305
Heloisa <helopessoa987@icloud.com>
2022-06-05 13:16:07 -0300

    Dice working without observer

--- src/view/DadosView.java
+++ src/view/DadosView.java
@@ -2,46 +2,27 @@ package view;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Observable;
 
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
 
 import model.FacadeModel;
 
-//@SuppressWarnings("deprecation")
-class DadosView extends JFrame{
+@SuppressWarnings("deprecation")
+class DadosView {//extends Observable{
 	private int resultados[] = new int[]{0,0};
 	private FacadeModel model = FacadeModel.getInstance();
-	String image1 = null;
-	String image2 = null;
+	String image1 = "die_face_1.png";
+	String image2 = "die_face_1.png";
 	String path = "Imagens-01/dados/";
-	private BufferedImage dadoImagem1;
-	private BufferedImage dadoImagem2;
+//	Tabuleiro tabuleiro = new Tabuleiro();
 	
 	DadosView(){
-		rodaDados();
-		setLayout(null);
-		this.setVisible(true);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		System.out.print(path+image1);
-		try {
-			dadoImagem1 = ImageIO.read(getClass().getResourceAsStream(path + image1));
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			dadoImagem2 = ImageIO.read(getClass().getResourceAsStream(path + image2));
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-
-		this.setSize((dadoImagem1.getWidth()*2 + 100), (dadoImagem1.getHeight() + 50));
+//		addObserver(tabuleiro);
+//		rodaDados();
 	}
 	
-	void rodaDados() {
+	String rodaDados() {
 		resultados = model.getDados(2);
 		Integer myInt1 = Integer.valueOf(resultados[0]);
 		String s1 = myInt1.toString();
@@ -49,13 +30,9 @@ class DadosView extends JFrame{
 		String s2 = myInt2.toString();
 		image1 = "die_face_" + s1 + ".png";
 		image2 = "die_face_" + s2 + ".png";
+		return image1+image2;
 //		setChanged();
 //		notifyObservers(image1+image2);
-	}
-	public void paint(Graphics g) {
-		Graphics2D g2D = (Graphics2D) g;
-		g2D.drawImage(dadoImagem1,20,35,dadoImagem1.getWidth(), dadoImagem1.getHeight(),null);
-		g2D.drawImage(dadoImagem2,360,35,dadoImagem2.getWidth(), dadoImagem2.getHeight(),null);
 	}
 
 }
--- src/view/Tabuleiro.java
+++ src/view/Tabuleiro.java
@@ -6,8 +6,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.Observable;
-import java.util.Observer;
+//import java.util.Observable;
+//import java.util.Observer;
 
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
@@ -16,17 +16,20 @@ import javax.swing.JFrame;
 public class Tabuleiro extends JFrame implements ActionListener{//,Observer{
 	
 	private BufferedImage image;
+	private String image1;
+	private String image2;
 	private BufferedImage dadoImagem1;
 	private BufferedImage dadoImagem2;
 	private JButton salvarJogo = new JButton("SALVAR");
 	private JButton carregarJogo = new JButton("CARREGAR");
 	private JButton rodaDados = new JButton("ROLL");
 	private JButton baralhoSorte = new JButton("BARALHO");
-	private DadosView dado;
+	private DadosView dado = new DadosView();
 	String path = "Imagens-01/dados/";
 	
 	public Tabuleiro() {
 		setLayout(null);
+		setImagesDado();
 		salvarJogo.setBounds(20,20,100,45);
 		carregarJogo.setBounds(120,20,100,45);
 		rodaDados.setBounds(320,550,75,45);
@@ -46,37 +49,58 @@ public class Tabuleiro extends JFrame implements ActionListener{//,Observer{
 		
 		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		System.out.println(image1);
 		try {
 			image = ImageIO.read(getClass().getResourceAsStream("Imagens-01/tabuleiro.png"));
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
-//		try {
-//			dadoImagem1 = ImageIO.read(getClass().getResourceAsStream(path + "die_face_1.png"));
-//		} catch(IOException e) {
-//			e.printStackTrace();
-//		}
-//		try {
-//			dadoImagem2 = ImageIO.read(getClass().getResourceAsStream(path + "die_face_1.png"));
-//		} catch(IOException e) {
-//			e.printStackTrace();
-//		}
+		try {
+			dadoImagem1 = ImageIO.read(getClass().getResourceAsStream(path + image1));
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			dadoImagem2 = ImageIO.read(getClass().getResourceAsStream(path + image2));
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 
 		this.setSize(image.getWidth()+20, image.getHeight()+115);
+	}
+	
+	void setImagesDado() {
+		String images = dado.rodaDados();
+//		System.out.println(images);
+		this.image1 = images.substring(0, 14);
+		System.out.println(image1);
+		this.image2 = images.substring(14);
+		System.out.println(image2);
+		try {
+			dadoImagem1 = ImageIO.read(getClass().getResourceAsStream(path + image1));
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			dadoImagem2 = ImageIO.read(getClass().getResourceAsStream(path + image2));
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void paint(Graphics g) {
 		Graphics2D g2D = (Graphics2D) g;
 		g2D.drawImage(image,10,105,image.getWidth(), image.getHeight(),null);
-//		g2D.drawImage(dadoImagem1,590,550,dadoImagem1.getWidth(), dadoImagem1.getHeight(),null);
-//		g2D.drawImage(dadoImagem2,590,580,dadoImagem2.getWidth(), dadoImagem2.getHeight(),null);
+		g2D.drawImage(dadoImagem1,280,490,70,70,null);
+		g2D.drawImage(dadoImagem2,380,490,70,70,null);
 	}
+	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == rodaDados) {
-//			dado.rodaDados();
-			this.dado = new DadosView();
+			this.setImagesDado();
+			this.repaint();
 		}
 		else if(e.getSource() == carregarJogo) {
 			// to be implemented
@@ -88,23 +112,16 @@ public class Tabuleiro extends JFrame implements ActionListener{//,Observer{
 		
 	}
 
+	
 //	@Override
 //	public void update(Observable o, Object arg1) {
-//		DadosView dado = (DadosView)o;
+////		DadosView dado = (DadosView)o;
 //		String images = String.valueOf(arg1);
 //		String image1 = images.substring(0, 10);
 //		String image2 = images.substring(10);
-//		try {
-//			dadoImagem1 = ImageIO.read(getClass().getResourceAsStream(path + image1));
-//		} catch(IOException e) {
-//			e.printStackTrace();
+//		setImages(image1,image2);
+//		this.repaint();    
 //		}
-//		try {
-//			dadoImagem2 = ImageIO.read(getClass().getResourceAsStream(path + image2));
-//		} catch(IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
 		
 
 }
